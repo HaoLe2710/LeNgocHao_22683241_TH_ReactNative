def4de803b791f4746aed0449c8f6d0e5e1b32eb
H82M19\Student <Student>
2025-08-27 07:20:22 +0700

    Bai 1 Tuan 02

--- src/bai1.ts
+++ src/bai1.ts
@@ -1,16 +1,5 @@
-export class Person{
-    name: string;
-    age: number;
-
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-
-    greet(): string {
-        return `Hello, my name is ${this.name}, ${this.age} years old`;
-    }
-}
-
-const person = new Person("Hao", 21);
-console.log(person.greet());
+const helloAsynv = new Promise(resolve =>{
+    setTimeout(() => {
+        resolve(console.log("Hello Async"))
+    },2000)
+})
