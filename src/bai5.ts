edb753919c1c18f604b8cccf489f0648f02eb8b4
H82M19\Student <Student>
2025-08-27 07:54:53 +0700

    Bai 5 Tuan 02

--- src/bai5.ts
+++ src/bai5.ts
@@ -1,26 +1,12 @@
-class BankAccount{
-    balance: number;
-
-    constructor(initialBalance: number) {
-        this.balance = initialBalance;
-    }
-
-    deposit(amount: number): void {
-        this.balance += amount;
-    }
-
-    withdraw(amount: number): void {
-        this.balance -= amount;
-    }
-
-    getBalance(): number {
-        return this.balance;
-    }
+function simulateTask(time:number): Promise<string>{
+    return new Promise(resolve =>{
+        setTimeout(() => {
+            resolve("Task done")
+        },time)
+    })
 }
 
-const myAccount = new BankAccount(1000);
-console.log(`Initial balance: ${myAccount.getBalance()}`);
-myAccount.deposit(500);
-console.log(`After deposit: ${myAccount.getBalance()}`);
-myAccount.withdraw(200);
-console.log(`After withdrawal: ${myAccount.getBalance()}`);
+simulateTask(3000).then(result => {
+    console.log(result);
+    
+})
