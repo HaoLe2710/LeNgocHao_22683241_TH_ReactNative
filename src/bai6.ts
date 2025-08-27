2f077285f175d6f88f8564c4ddc2932b58521b3c
H82M19\Student <Student>
2025-08-27 08:02:54 +0700

    Bai 6 Tuan 02

--- src/bai6.ts
+++ src/bai6.ts
@@ -1,11 +1,7 @@
-class Book{
-    title: string;
-    author:string;
-    year:number;
+const pr1 = Promise.resolve("Le Ngoc Hao")
+const pr2 = new Promise(resolve =>{setTimeout(() => resolve(22683241)),3000})
+const pr3 = new Promise((resolve) => setTimeout(() =>{resolve("End task")},5000))
 
-    constructor(title: string, author: string, year: number) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
-}
+Promise.all([pr1,pr2,pr3]).then(result =>{console.log(result)
+}).catch(error => {console.log(error);
+})
