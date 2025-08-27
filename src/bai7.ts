5e783436a05e13ef887161668dfc5771a501bdaf
H82M19\Student <Student>
2025-08-27 08:11:49 +0700

    Bai 7 Tuan 02

--- src/bai7.ts
+++ src/bai7.ts
@@ -1,15 +1,6 @@
-class User{
-    private name: string;
+const pr4 = new Promise(resolve => { setTimeout(() => {resolve("Hello")},1000)})
+const pr5 = new Promise(resolve => { setTimeout(() => {resolve("That's good"),500})})
 
-    constructor(name: string) {
-        this.name = name;
-    }
-
-    getName(): string {
-        return this.name;
-    }
-
-    setName(name: string): void {
-        this.name = name;
-    }
-}
+Promise.race([pr4,pr5]).then(result => {console.log(result);
+}).catch(error =>{ console.log(error);
+})
