09b4eed466a6e52a91821de0bc7dc441c74ea0f1
H82M19\Student <Student>
2025-08-27 08:29:49 +0700

    Bai 10 Tuan 02

--- src/bai10.ts
+++ src/bai10.ts
@@ -1,11 +1,13 @@
-class Account{
-    public name:string;
-    private password:string;
-    readonly createdAt: Date;
-
-    constructor(name: string, password: string) {
-        this.name = name;
-        this.password = password;
-        this.createdAt = new Date();
-    }
+function filterOddNumberWithPromise(nums: number[]){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+           const evenNums = nums.filter(num => num%2 != 0)
+           resolve(evenNums)
+        },1000)
+    })
 }
+
+filterOddNumberWithPromise([1,2,3,4,5])
+.then(result => console.log(result))
+.catch(error => console.log(error))
+.finally(() => console.log('DONE !'))
