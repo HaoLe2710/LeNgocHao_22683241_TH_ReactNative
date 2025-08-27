0fdae602efe9e13700a3e7b7b2a6efda7e47e867
H82M19\Student <Student>
2025-08-27 08:26:19 +0700

    Bai 9 Tuan02

--- src/bai9.ts
+++ src/bai9.ts
@@ -1,4 +1,10 @@
-interface Animal{
-    name:string;
-    sound():string;
+function filterEvenNumberWithPromise(nums: number[]){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+           const evenNums = nums.filter(num => num%2 == 0)
+           resolve(evenNums)
+        },1000)
+    })
 }
+
+filterEvenNumberWithPromise([1,2,3,4,5]).then(result => console.log(result)).catch(error => console.log(error))
