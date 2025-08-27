faf00ccbaee227bf0f858f708c56491238e11b6d
H82M19\Student <Student>
2025-08-27 07:32:13 +0700

    Bai 2 Tuan 02

--- src/bai2.ts
+++ src/bai2.ts
@@ -1,17 +1,15 @@
-import {Person} from "./bai1";
-
-class Student extends Person{
-    grade:number;
-
-    constructor(name: string, age: number, grade: number) {
-        super(name, age);
-        this.grade = grade;
-    }
-
-    study(): string {
-       return ` and now I am studying for grade ${this.grade}.`;
-    }
+function promiseWithNumber(){
+    return new Promise<number>(resolve => {
+        setTimeout(() => {
+            resolve(10)
+        },1000)
+    })
 }
 
-const student = new Student("Hao", 21, 12);
-console.log(student.study());
+promiseWithNumber().then((result) =>{
+    console.log(result);
+    
+})
+
+
+
