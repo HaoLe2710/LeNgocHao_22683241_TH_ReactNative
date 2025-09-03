b2d7477ae68293ea9a0a98db11dc22472d1ad340
HaoLe2710 <lengochao123.27102004@gmail.com>
2025-09-03 08:52:52 +0700

    giao dien 3

--- ThucHanhTuan03/app/(tabs)/index.tsx
+++ ThucHanhTuan03/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { Text, SafeAreaView, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // You can import supported modules from npm
@@ -14,29 +14,30 @@ export default function App() {
     <SafeAreaProvider style={styles.container}>
      
       <SafeAreaView style={{flex: 3, alignItems:'center'}}>
-        <Image source={require('@/assets/images/logo1.png')} style={{width: 200, height: 200, alignSelf: 'center'}} />
+        <Image source={require('@/assets/images/lock.png')} style={{width: 200, height: 200, alignSelf: 'center'}} />
       </SafeAreaView>
       <SafeAreaView style={{flex: 1}}>
         <Text style={styles.title}>
-          GROW YOUR BUSINESS
+          FORGET PASSWORD
         </Text>
       </SafeAreaView>
       <SafeAreaView style={{flex: 1}}>
         <Text style={styles.paragraph}>
-          We will help you growing your business using online server
+          Product your account's email for which you want to reset your password
         </Text>
+      </SafeAreaView>
+      <SafeAreaView style={styles.input_container}>
+        <Image source={require('@/assets/images/email.png')} style={styles.icon} />
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="#555"
+            style={styles.input}
+            keyboardType="email-address"
+          />
       </SafeAreaView>
       <SafeAreaView style={{flex: 1, flexDirection: 'row', gap: 100, alignItems: 'center'}}>
        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>SIGN UP</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
-      <SafeAreaView>
-        <TouchableOpacity>
-          <Text style={{fontSize: 20, textAlign: 'center', fontWeight: 'bold'}}>HOW WE WORK</Text>
+          <Text style={styles.text}>NEXT</Text>
         </TouchableOpacity>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -52,7 +53,7 @@ const styles = StyleSheet.create({
   },
   paragraph: {
     margin: 24,
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: 'bold',
     textAlign: 'center',
   },
@@ -80,5 +81,35 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     flex: 0
-  }
+  },
+  text_input:{
+    height: 50,
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 5
+  },
+  input_container:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: '#888',
+    borderRadius: 5,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    marginBottom: 30,
+    backgroundColor: '#fff',
+  },
+  icon:{
+    width: 24,
+    height: 24,
+    marginRight: 10,
+    marginLeft: 10,
+    tintColor: '#444',
+  },  
+  input:{
+    flex: 1,
+    height: 40,
+    fontSize: 16,
+    color: '#000',
+  },
 });
